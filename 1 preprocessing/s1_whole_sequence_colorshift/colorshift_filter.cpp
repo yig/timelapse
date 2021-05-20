c98e85cd13e2dfdfff5e32cf27292521e1996e42
Yotam Gingold <yotam@yotamgingold.com>
2021-05-20 14:02:31 -0400

    working on modern cmake

--- 1 preprocessing/s1_whole_sequence_colorshift/colorshift_filter.cpp
+++ 1 preprocessing/s1_whole_sequence_colorshift/colorshift_filter.cpp
@@ -4,7 +4,7 @@
 #include <opencv2/highgui/highgui.hpp> 
 #include <opencv2/core/core.hpp> 
 #include <opencv2/imgproc/imgproc.hpp>
-#include <opencv/cv.hpp>
+#include <opencv2/opencv.hpp>
 #include <opencv2/features2d/features2d.hpp>
 #include <opencv2/calib3d/calib3d.hpp>
 //#include <open nonfree/features2d.hpp>
